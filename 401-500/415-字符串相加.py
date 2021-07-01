cd44982b5dd1160e5aaa5f422d90946f43419632
tyutltf <867758386@qq.com>
2021-07-01 15:16:11 +0800

    新增

---
+++ 401-500/415-字符串相加.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    :   415-字符串相加.py
+@Time    :   2021/06/22 23:00:38
+@Author  :   TYUT ltf
+@Version :   v1.0
+@Contact :   18235121656@163.com
+@License :   (C)Copyright 2020-2030, GNU General Public License
+"""
+# here put the import lib
+
+"""
+给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+
+ 
+
+提示：
+
+num1 和num2 的长度都小于 5100
+num1 和num2 都只包含数字 0-9
+num1 和num2 都不包含任何前导零
+你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式
+
+"""
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str):
+        res = ""
+        i, j, carry = len(num1) - 1, len(num2) - 1, 0
+        while i >= 0 or j >= 0:
+            n1 = int(num1[i]) if i >= 0 else 0
+            n2 = int(num2[j]) if j >= 0 else 0
+            tmp = n1 + n2 + carry
+            carry = tmp // 10
+            res = str(tmp % 10) + res
+            i, j = i - 1, j - 1
+        return "1" + res if carry else res
+
+
+num1 = "111"
+num2 = "1234"
+obj = Solution()
+print(obj.addStrings(num1, num2))
